4de79940eed0e9868dc29ffc2f5af848703d4444
Brooke Kuhlmann <brooke@redalchemist.com>
2011-10-11 19:34:55 -0600

    Removed namespace placeholder.

--- lib/db.rb
+++ lib/db.rb
@@ -4,8 +4,3 @@ require File.join File.dirname(__FILE__), "db", "utilities.rb"
 require File.join File.dirname(__FILE__), "db", "rails.rb"
 require File.join File.dirname(__FILE__), "db", "pg.rb"
 
-# Namespace
-module DB
-  # Placeholder.
-end
-
