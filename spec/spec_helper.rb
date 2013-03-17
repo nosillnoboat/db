43df4e62437ef767e6f9e51a26df9bd297330815
Brooke Kuhlmann <brooke@redalchemist.com>
2013-03-17 15:10:17 -0600

    Added Pry development support. [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require "db"
 require File.join "db", "rails"
 require File.join "db", "pg"
 require File.join "db", "cli"
+require "pry"
 
 RSpec.configure do |config|
   config.filter_run focus: true
